d79850637bcc98bff9e4d60df1a193dd2df15c2c
Christopher Harrison <geektrainer@github.com>
2022-07-25 16:04:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.Sqlite;
+
+// connect to sqlite database
+var connection = new SqliteConnection("Data Source=test.db");
+connection.Open();
+
+// create table with first name and last name
+var command = connection.CreateCommand();
+command.CommandText = "CREATE TABLE IF NOT EXISTS people (first_name TEXT, last_name TEXT)";
+command.ExecuteNonQuery();
